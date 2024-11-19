6985df5d7f45790bfeedad0c85153788942172cb
CHOI DAE GEON <cdg@aplayz.co.kr>
2024-11-20 08:35:00 +0900

    typescript 함수 진행중

---
+++ src/function.js
@@ -0,0 +1,43 @@
+"use strict";
+//함수 타입
+function add(x, y) {
+    return x + y;
+}
+let myAdd = function (x, y) {
+    return x + y;
+};
+//전체 함수 타입 작성
+// 함수 타입의 경우 매개변수 타입과 반환 타입이 존재
+let myAdd2 = function (x, y) {
+    return x + y;
+};
+//매개변수들의 타입이 올바르게 되어있다면,함수타입에 이름이 다르더라도 유효하다
+let myAdd3 = function (x, y) {
+    return x + y;
+};
+//선택적 매개변수
+function buildName(firstName, lastName) {
+    if (lastName) {
+        return firstName + " " + lastName;
+    }
+    else {
+        return firstName;
+    }
+}
+let result1 = buildName("Bob");
+let result2 = buildName("Bob", "Adams");
+//기본 초기화 매개변수
+// 기본값을 정의하고 싶을때 사용된다
+function buildName2(firstName, lastName = "CHOI") {
+    return firstName + " " + lastName;
+}
+let buildName2Result1 = buildName2("Bob");
+let buildName2Result2 = buildName2("Bob", "TEST");
+let buildName2Result3 = buildName2("Bob", undefined);
+// 나머지 매개변수
+// 필수 1개와 다수의 매개변수를 받을때
+function buildName3(firstName, ...restOfName) {
+    return firstName + " " + restOfName.join(" ");
+}
+let employeeName = buildName3("Joseph", "Name1", "Name2", "Name3");
+//
---
+++ src/function.ts
@@ -0,0 +1,57 @@
+//함수 타입
+function add(x: number, y: number): number {
+  return x + y;
+}
+
+let myAdd = function (x: number, y: number): number {
+  return x + y;
+};
+
+//전체 함수 타입 작성
+// 함수 타입의 경우 매개변수 타입과 반환 타입이 존재
+let myAdd2: (x: number, y: number) => number = function (
+  x: number,
+  y: number
+): number {
+  return x + y;
+};
+
+//매개변수들의 타입이 올바르게 되어있다면,함수타입에 이름이 다르더라도 유효하다
+let myAdd3: (baseValue: number, increment: number) => number = function (
+  x: number,
+  y: number
+): number {
+  return x + y;
+};
+
+//선택적 매개변수
+function buildName(firstName: string, lastName?: string) {
+  if (lastName) {
+    return firstName + " " + lastName;
+  } else {
+    return firstName;
+  }
+}
+
+let result1 = buildName("Bob");
+let result2 = buildName("Bob", "Adams");
+
+//기본 초기화 매개변수
+// 기본값을 정의하고 싶을때 사용된다
+
+function buildName2(firstName: string, lastName = "CHOI") {
+  return firstName + " " + lastName;
+}
+let buildName2Result1 = buildName2("Bob");
+let buildName2Result2 = buildName2("Bob", "TEST");
+let buildName2Result3 = buildName2("Bob", undefined);
+
+// 나머지 매개변수
+// 필수 1개와 다수의 매개변수를 받을때
+function buildName3(firstName: string, ...restOfName: string[]) {
+  return firstName + " " + restOfName.join(" ");
+}
+
+let employeeName = buildName3("Joseph", "Name1", "Name2", "Name3");
+
+//
