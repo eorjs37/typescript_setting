84718f04792cf340e7d9fe24e872fec453e7214a
CHOI DAE GEON <cdg@aplayz.co.kr>
2024-12-31 08:50:58 +0900

    Never type 처리

--- src/type.js
+++ src/type.js
@@ -1,17 +1,17 @@
 "use strict";
 //문자열
-let str = 'hello';
+let str = "hello";
 //숫자
 let num = 123;
 //불린
 let bool = true;
 //객체
-let user = { name: 'choi', age: 32 };
+let user = { name: "choi", age: 32 };
 //배열
 let arr = [1, 2, 3];
 let arrgen = [1, 2, 3];
 //튜플(배열의 길이는 고정되고, 각 요소의 타입이 지정되어있는 형식)
-let tuple = ['1', 2];
+let tuple = ["1", 2];
 //정의되지 않는 타입은 오류
 //tuple[5]=1;
 //tuple[1].concat('1')
@@ -26,9 +26,13 @@ let hero = Avengers.Capt;
 //any(모든 타입에 사용할 수 있는 타입,any를 사용하면 타입추론이 힘듬)
 //void(반환값이 없는 함수의 반환 타입)
 function pringSomething() {
-    console.log('sth');
+    console.log("sth");
 }
 function pringSomething2() {
     return;
 }
 //never(절대 발생하지 않는 값을 의미)
+function invalid(message) {
+    throw Error(message);
+}
+invalid("error");
