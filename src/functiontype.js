564737de8c3b2c5ff1ceecf520c29572f5030f6a
CHOI DAE GEON <cdg@aplayz.co.kr>
2025-01-13 08:29:30 +0900

    this 진행중

--- src/functiontype.js
+++ src/functiontype.js
@@ -50,3 +50,16 @@ let emoloyeeName = buildName3("cho", "1", "2", "3");
 let buildNameFn = function (firstName, ...restOfName) {
     return firstName + " " + restOfName.join(" ");
 };
+let deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    createCardPicker: function () {
+        return () => {
+            let pickedCard = Math.floor(Math.random() * 52);
+            let pickedSuit = Math.floor(pickedCard / 13);
+            return { suit: this.suits[pickedSuit], card: pickedCard % 13 };
+        };
+    },
+};
+let cardPicker = deck.createCardPicker();
+let pickedCard = cardPicker();
--- src/functiontype.ts
+++ src/functiontype.ts
@@ -66,4 +66,30 @@ let buildNameFn: (fname: string, ...rest: string[]) => string = function (
 ) {
   return firstName + " " + restOfName.join(" ");
 };
+
+interface Card {
+  suit: string;
+  card: number;
+}
+
+interface Deck {
+  suits: string[];
+  cards: number[];
+  createCardPicker(this: Deck): () => void;
+}
+
+let deck = {
+  suits: ["hearts", "spades", "clubs", "diamonds"],
+  cards: Array(52),
+  createCardPicker: function () {
+    return () => {
+      let pickedCard = Math.floor(Math.random() * 52);
+      let pickedSuit = Math.floor(pickedCard / 13);
+
+      return { suit: this.suits[pickedSuit], card: pickedCard % 13 };
+    };
+  },
+};
+let cardPicker = deck.createCardPicker();
+let pickedCard = cardPicker();
 export {};
