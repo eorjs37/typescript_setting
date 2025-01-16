4a43c3b89178203468f80cee972ba5df97046f36
CHOI DAE GEON <cdg@aplayz.co.kr>
2025-01-17 07:49:36 +0900

    제네릭 진행중

--- src/genric3.ts
+++ src/genric3.ts
@@ -22,4 +22,102 @@ interface PersonGenric<N, A> {
 
 interface Person3 extends PersonGenric<"choi", 32> {}
 interface Person4 extends PersonGenric<"choi2", 28> {}
+
+//함수 표현식
+const personFactoryE = <N, A>(name: N, age: A) => ({
+  type: "human",
+  race: "yellow",
+  name,
+  age,
+});
+
+//함수 선언문
+function personFactoryD<N, A>(name: N, age: A) {
+  return {
+    type: "human",
+    race: "yellow",
+    name,
+    age,
+  };
+}
+
+class Person<N, A> {
+  name: N;
+  age: A;
+  constructor(name: N, age: A) {
+    this.name = name;
+    this.age = age;
+  }
+  method<B>(param: B) {}
+}
+
+interface Person5<N, A> {
+  type: "human";
+  race: "yellow";
+  name: N;
+  age: A;
+}
+
+const personFactoryF = <N, A = unknown>(name: N, age: A): Person5<N, A> => {
+  return {
+    type: "human",
+    race: "yellow",
+    name,
+    age,
+  };
+};
+
+//타입추론이 가능하다
+const zero = personFactoryF("zero", 11);
+
+//상수 타입 매개변수
+function values<T>(initial: T[]) {
+  return {
+    hasValue(value: T) {
+      return initial.includes(value);
+    },
+  };
+}
+
+const savedValue = values(["a", "b", "c"]);
+savedValue.hasValue("x");
+
+function values2<const T>(initial: T[]) {
+	return {
+		hasValue(value: T) {
+			return initial.includes(value)
+		}
+	}
+}
+
+const savedValue2 = values2(["a", "b", "c"])
+//savedValue2.hasValue("x")
+
+interface Example<A extends number, B = string>{
+	a: A,
+	b:B
+}
+//error
+//type UseCase1 = Example<string,boolean>
+type UseCase2 = Example<1, boolean>
+type UseCase3 = Example<number>
+
+interface Example2<A, B extends A>{
+	a: A,
+	b:B
+}
+
+//type UseCase4 = Example2<string, number>
+type UseCase5 = Example2<string, 'hello'>
+type UseCase6 = Example2<number, 123>
+
+
+interface VO{
+	value:any
+}
+
+
+function onlyBoolena(arg: true | false = true): true | false{
+	return arg;
+}
 export {};
