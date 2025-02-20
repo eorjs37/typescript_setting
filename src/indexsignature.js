a5240115e60f158d10cae64015dac0adc709ef95
CHOI DAE GEON <cdg@aplayz.co.kr>
2025-02-20 09:02:55 +0900

    fix: 인덱스 시그니처 처리

---
+++ src/indexsignature.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const obj = {
+    a: 200,
+    b: 300,
+    c: 400,
+};
+function total() { }
---
+++ src/indexsignature.ts
@@ -0,0 +1,11 @@
+const obj = {
+  a: 200,
+  b: 300,
+  c: 400,
+};
+
+type ObjKey = typeof obj;
+
+function total() {}
+
+export {};
