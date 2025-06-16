3dcdd028d3156238ba81e782a2bcd7925563743c
CHOI DAE GEON <cdg@aplayz.co.kr>
2025-06-17 08:18:11 +0900

    컨디셔널 타입

--- src/conditionalType3.ts
+++ src/conditionalType3.ts
@@ -61,4 +61,29 @@ type OmitByType3<O, T> = {
 };
 type OM3 = OmitByType3<MappingType, string>;
 
+type ChooseArray1<A> = A extends string
+  ? string[]
+  : A extends boolean
+  ? boolean[]
+  : never;
+
+type StringArray1 = ChooseArray1<string>;
+type NeverArray1 = ChooseArray1<number>;
+
+type Start1 = string | number;
+type Result1 = Start extends string ? Start[] : never;
+
+type Result2<Key> = Key extends string ? Key[] : never;
+let n: Result2<Start1> = ["1"];
+
+type IsString<T> = T extends string ? true : false;
+type Result3 = IsString<"hi" | 3>;
+
+type IsString2<T> = [T] extends [string] ? true : false;
+type Result4 = IsString2<"hi" | 3>;
+
+function test<T>(a: T) {
+  type R<T> = T extends string ? T : T;
+  // const b: R<T> = a;
+}
 export {};
