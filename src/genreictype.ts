364007c32ad9cb03a797323177edd02dc1abcd41
CHOI DAE GEON <cdg@aplayz.co.kr>
2025-07-25 08:53:28 +0900

    제네릭 타입 추가

--- src/genreictype.ts
+++ src/genreictype.ts
@@ -14,4 +14,45 @@ const personFactoryE = <N, A>(name: N, age: A) => ({
   name,
   age,
 });
+
+function personFactoryD<N, A>(name: N, age: A) {
+  return {
+    type: "human",
+    race: "yellow",
+    name,
+    age,
+  };
+}
+
+interface Person2<N = string, A = number> {
+  type: "human";
+  race: "yellow";
+  name: N;
+  age: A;
+}
+
+type Pesron3 = Person2;
+type Pesron4 = Person2<number>;
+type Pesron5 = Person2;
+
+
+
+function values<const T>(initial: T[]){
+	return {
+		hasValues(value: T) {
+			return initial.includes(value)
+		}
+	}
+}
+
+const savedValue = values(["a","b","c"])
+savedValue.hasValues("a")
+
+interface VO{
+	value: any;
+}
+
+const returnVo = <T extends VO>() => {
+	return null
+}
 export {};
